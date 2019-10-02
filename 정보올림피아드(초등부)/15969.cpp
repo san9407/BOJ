cd9a10b6d980d9380e03183aeb11cf21eec7be93
san9407 <38039468+san9407@users.noreply.github.com>
2019-10-02 22:58:51 +0900

    Create 15969.cpp

---
+++ 정보올림피아드(초등부)/15969.cpp
@@ -0,0 +1,31 @@
+#include <cstdio>
+#include <cstring>
+#include <cmath>
+#include <algorithm>
+#include <queue>
+#include <vector>
+#include <map>
+#include <set>
+#include <stack>
+#include <functional>
+#include <tuple>
+using namespace std;
+#pragma warning(disable:4996)
+const int INF = 1000000001;
+const long long LINF = 1e18 + 1;
+typedef long long ll;
+
+int arr[1000];
+
+int main()
+{
+	int N;
+	scanf("%d", &N);
+	for (int i = 0; i < N; i++)
+	{
+		scanf("%d", arr + i);
+	}
+	sort(arr, arr + N);
+	printf("%d\n", arr[N - 1] - arr[0]);
+	return 0;
+}
